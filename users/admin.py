d138923e3d3344e0f38afd98da248d5c6b34f686
pragat-pandya <pragat.pandya@gmail.com>
2022-03-08 12:34:50 +0530

    Define User model in users app
    TODO: Register Login and Logout
    TODO: Forgot Password and Reset Password.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Account
+from django.contrib.auth.admin import UserAdmin
+from django.utils.html import format_html
+
+
+
+class AccountAdmin (UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
+    list_display_links = ('email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('-date_joined',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
 
 # Register your models here.
+admin.site.register (Account, AccountAdmin)
